1a4c0a0f716d36384c99636eb497ffd63fbc0e37
oog1 <oog@novicell.dk>
2025-04-11 13:26:55 +0200

    fixed Tailwind CSS IntelliSense problem, and added default layout to all pages

--- src/app.vue
+++ src/app.vue
@@ -4,10 +4,7 @@
 
 <template>
   <div>
-    <nav class="p-4 bg-gray-100 ">
-      <NuxtLink to="/" class="mr-4">Home</NuxtLink>
-      <NuxtLink to="/about">About</NuxtLink>
-    </nav>
+   
 
     <NuxtLayout>
         <NuxtPage />
