6beab377d1f646c0cf7748018ae11266ba067d9e
oog1 <oog@novicell.dk>
2025-04-22 14:30:08 +0200

    new partion component

---
+++ src/components/partial/AccountNavigation.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface NavigationItem {
+  NavName: string
+}
+
+const navigationName = ref<NavigationItem[]>([
+  { NavName: 'Min Side' },
+  { NavName: 'Profile' },
+  { NavName: 'Favorite' },
+  { NavName: 'Ordrehistorik' },  
+])
+</script>
+
+<template>
+
+   <nav>
+    <ul></ul>
+   </nav>
+</template>
