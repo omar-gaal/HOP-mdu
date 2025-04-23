e3ae6f4fcd85648852bf136a613af8c4fa04e5d7
oog1 <oog@novicell.dk>
2025-04-23 12:44:58 +0200

    added function to check route to add our orange color on the NavName

--- src/components/partial/AccountNavigation.vue
+++ src/components/partial/AccountNavigation.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useRoute } from '#app';
+
+
+const route = useRoute();
 
 interface NavigationItem {
-  NavName: string
-  href: string
+  NavName: string;
+  href: string;
 }
 
 const navigationName = ref<NavigationItem[]>([
-  { NavName: 'Min Side', href: '/konto' },
+  { NavName: 'Min Side', href: '/konto'},
   { NavName: 'Profil',  href: '/konto/profile' } ,
-  { NavName: 'Favoritter',  href: '/konto/favoriter' },
-  { NavName: 'Ordrehistorik', href: '/konto/orders' },
+  { NavName: 'Favoritter',  href: '/konto/favoriter'},
+  { NavName: 'Ordrehistorik', href: '/konto/orders'},
  
 
 ])
@@ -19,11 +23,12 @@ const navigationName = ref<NavigationItem[]>([
 <template>
   <div class="bg-primary   text-white w-full max-w-screen-lg mx-auto">
     <ul class=" py-5 gap-9 sm:gap-0 px-5 flex flex-col md:flex-row items-left md:items-center justify-center text-xl md:text-2xl">
-      <a 
+      <NuxtLink 
         v-for="(item, index) in navigationName" 
         :key="index"
-        :href="item.href"
-        class="flex flex-col md:flex-row md:items-center md:gap-24 md:px-6 py-2 hover:text-secondary "
+        :to="item.href"
+        class="flex flex-col md:flex-row md:items-center md:gap-24 md:px-6 py-2 hover:text-secondary"
+        :class="{ 'text-secondary': route.path === item.href }"
       >
         <span>{{ item.NavName }}</span>
         
@@ -35,7 +40,7 @@ const navigationName = ref<NavigationItem[]>([
 
       
         <div class="md:hidden sm:w-3/4 w-full border-t border-white mt-2"></div>
-      </a>
+      </NuxtLink>
     </ul>
   </div>
 </template>
