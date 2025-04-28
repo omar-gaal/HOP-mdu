0473dd982b03fe38888215c2971e53f531e6e942
oog1 <oog@novicell.dk>
2025-04-28 18:00:58 +0200

    fix routing to a 404 page now to where it's needed

--- src/Navigation/DesktopNav.vue
+++ src/Navigation/DesktopNav.vue
@@ -13,8 +13,8 @@
     <div class="flex items-center gap-6">
       <nav class="text-white text-lg flex gap-4">
         <NuxtLink to="/">Produkter</NuxtLink>
-        <NuxtLink to="/about">Services</NuxtLink>
-        <NuxtLink to="/about">Om os</NuxtLink>
+        <NuxtLink to="/page-not-found">Services</NuxtLink>
+        <NuxtLink to="/page-not-found">Om os</NuxtLink>
       </nav>
         <img src="../assets/icon/fav-w.png" alt="heart icon" class="w-6 h-6" />
       <BaseLoginBtn />
