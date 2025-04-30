0bff640de45f4fb6e0ea67d3efa26d9d5a5091d6
oog1 <oog@novicell.dk>
2025-04-30 16:00:14 +0200

    added initials in BaseinitialIcon.vue

--- src/components/base/BaseInitialIcon.vue
+++ src/components/base/BaseInitialIcon.vue
@@ -1,9 +1,14 @@
-<script lang="ts">
-
+<script setup lang="ts">
+defineProps<{
+  initials?: string
+}>();
 </script>
 
 
 
 <template>
-    <div class="bg-red-500 w-12 h-12"></div>
-</template>
+    <div class="bg-white w-12 h-12 rounded-full flex items-center justify-center text-black font-bold uppercase">
+       
+      <span class="text-black">{{ initials }}</span>
+    </div>
+  </template>
