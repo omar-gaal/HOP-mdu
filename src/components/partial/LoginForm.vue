45549e55d15444f98e8441113efefe3a99be1c1e
oog1 <oog@novicell.dk>
2025-04-30 14:07:59 +0200

    added some console log to check if the user is log in work and change the label to mail -> username cause the backend says to log in with userName and password

--- src/components/partial/LoginForm.vue
+++ src/components/partial/LoginForm.vue
@@ -6,14 +6,18 @@ import { useAuthStore } from '@/stores/auth';
 const modal = useModal();
 const authStore = useAuthStore();
 
-const email = ref('');
+// const email = ref('');
+const userName = ref('')
 const password = ref('');
 
 async function handleLogin() {
-  const success = await authStore.login(email.value, password.value);
+  const success = await authStore.login(userName.value, password.value);
+  console.log("login attempt result:", success)
   if (success) {
+    console.log("Login successful, user data:", authStore.user);
     modal.close();
   } else {
+    console.warn(" Login failed");
     alert('Fejl ved login. Prøv igen.');
   }
 }
@@ -34,10 +38,10 @@ async function handleLogin() {
 
         <form @submit.prevent="handleLogin" class="space-y-4">
           <div>
-            <label for="email" class="block text-sm font-medium mb-1">Mailadresse</label>
+            <label for="email" class="block text-sm font-medium mb-1">Brugernavn</label>
             <input
-              v-model="email"
-              id="email"
+              v-model="userName"
+              id="userName"
               type="email"
               class="w-full px-4 py-2 rounded border border-gray-300 bg-transparent text-white placeholder-gray-400"
               placeholder="Indtast din mail"
@@ -48,7 +52,7 @@ async function handleLogin() {
             <input
               v-model="password"
               id="password"
-              type="password"
+              type="text"
               class="w-full px-4 py-2 rounded border border-gray-300 bg-transparent text-white placeholder-gray-400"
               placeholder="Indtast adgangskode"
             />
