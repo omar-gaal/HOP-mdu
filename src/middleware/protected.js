3e310779df2ad1d51da492d242181462fc5835ce
oog1 <oog@novicell.dk>
2025-04-30 23:46:34 +0200

    adjusted middleware so that user that are log in can go to page where they have to otherwise they can't

--- src/middleware/protected.js
+++ src/middleware/protected.js
@@ -1,10 +1,6 @@
+import { useAuthStore } from "#imports"
 export default defineNuxtRouteMiddleware(async (to) => {
-    if (import.meta.server) return
-    const authStore =  useAuthStore()
-    await authStore.checkAuth()
-
-    // If user is not authenticated and trying to access a protected route
-    if (!authStore.user) {
+    if (!useAuthStore) {
       return navigateTo('/')
     }
   })
