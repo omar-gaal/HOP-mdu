953bd3fe2cb4278ae6a2850fb71ce5ad8e8d7cb5
oog1 <oog@novicell.dk>
2025-05-01 18:45:23 +0200

    tried add cookies to store usage when reload the page, still not working will come back to it later

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
 
 import { useAuthStore } from '#imports';
-onMounted(() => {
+onMounted(async () => {
   const auth = useAuthStore();
   auth.init();
+
+  const authCookie = useCookie('auth');
+if (authCookie.value) {
+  const result = await auth.checkAuth();
+  if (result) {
+    console.log('testing true');
+  }
+}
 });
+
+
+
 </script>
 
 <template>
