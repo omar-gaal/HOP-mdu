7412c3c28d3f1e3314ed555c5ea131fe2ed0238e
HaniBassam <143654847+HaniBassam@users.noreply.github.com>
2025-05-08 10:56:17 +0200

    removed ekstra info on basecardinfo cause it was not following design

--- src/pages/konto/index.vue
+++ src/pages/konto/index.vue
@@ -20,32 +20,30 @@ const logout = async () => {
 </script>
 
 <template>
-<BaseContainer :is-mypage="true">
-  <div class="flex justify-between py-8 ">
-    <h2 class="text-2xl">Hej {{userName || 'Bruger'}}</h2>
-    <button @click="logout" class="text-2xl underline" >Log ud </button>
-  </div>
-  <PartialAccountNavigation class="mb-24"/>
-  <div class="flex flex-col gap-6 text-white sm:flex-row sm:justify-between">
-    <BaseInfoCard
-       :title="'Mine detaljer'"
-       :userName="userName"
-       :email="auth.user?.email"
-       :text="'hej med dig'"
-       linkText="Mine detaljer"
-       linkUrl="/konto/profile"
-    />
-    <BaseInfoCard
-  title="Mine oplysninger"
-  :userName="profileStore.name"
-  :email="profileStore.email"
-  :adresse="profileStore.adresse"
-  :postNr="profileStore.postNr"
-  :by="profileStore.by"
-  :land="profileStore.land"
-  linkText="Rediger"
-  linkUrl="/profile"
-/>
-  </div>
-</BaseContainer>
+  <BaseContainer :is-mypage="true">
+    <div class="flex justify-between py-8">
+      <h2 class="text-2xl">Hej {{ userName || "Bruger" }}</h2>
+      <button @click="logout" class="text-2xl underline">Log ud</button>
+    </div>
+    <PartialAccountNavigation class="mb-24" />
+    <div class="flex flex-col gap-6 text-white sm:flex-row sm:justify-between">
+      <BaseInfoCard
+        :title="'Mine detaljer'"
+        :userName="userName"
+        :email="auth.user?.email"
+        :text="'hej med dig'"
+        linkText="Mine detaljer"
+        linkUrl="/konto/profile"
+      />
+      <BaseInfoCard
+        title="Mine oplysninger"
+        :adresse="profileStore.adresse"
+        :postNr="profileStore.postNr"
+        :by="profileStore.by"
+        :land="profileStore.land"
+        linkText="Rediger"
+        linkUrl="/profile"
+      />
+    </div>
+  </BaseContainer>
 </template>
