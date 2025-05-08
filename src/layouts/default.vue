2b5bfde179c1cf869c56050616fb56d2c7546997
oog1 <oog@novicell.dk>
2025-05-08 12:31:11 +0200

    removed some comment, and adjusted the mobile header to also show up on Ipad

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
 import DesktopNav from "~/Navigation/DesktopNav.vue";
 import MobileNav from "~/Navigation/MobileNav.vue";
-import TheFooter from "~/components/global/TheFooter.vue"; // make sure this import exists
+import TheFooter from "~/components/global/TheFooter.vue";
 import { useWindowSize } from "#imports";
 import { useAuthStore } from '@/stores/auth';
 
 const { width } = useWindowSize();
-const isMobile = computed(() => width.value < 768);
+const isMobile = computed(() => width.value < 1080);
 </script>
 
 <template>
-  <!-- Set up full-height flex layout -->
+
   <div class="min-h-screen flex flex-col">
 
-    <!-- Navigation -->
+  
     <div class="sticky top-0 z-50 bg-primary shadow-md">
       <DesktopNav  v-if="!isMobile" />
       <MobileNav v-else />
     </div>
 
-    <!-- Page content grows to fill space -->
+   
     <main class="flex-grow">
       <slot />
     </main>
 
-    <!-- Footer sticks to bottom if content is short -->
+    
     <TheFooter />
   </div>
 </template>
