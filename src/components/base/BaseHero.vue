7ac3a2394e938c363a350475ee99c891b564c041
oog1 <oog@novicell.dk>
2025-05-08 12:10:02 +0200

    Adjusted the responsiv layout for text and button on hero image on front page

--- src/components/base/BaseHero.vue
+++ src/components/base/BaseHero.vue
@@ -21,7 +21,7 @@ function scrollToProducts() {
     :class="'w-full object-cover relative h-[770px]  sm:h-[999px] brightness-75'"
   />
 
-  <div class="flex flex-col gap-4 absolute bottom-20 left-10 pl-30">
+  <div class="flex flex-col gap-4  absolute bottom-40 md:bottom-44 2xl:bottom-96   left-10 pl-30">
     <h1 class="text-4xl text-white">Professionel elektronik</h1>
     <span class="text-2xl text-white">direkte til din forretning</span>
 
