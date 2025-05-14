20b6bbb90dfd79820b5e8866d70a4631109f60a5
oog1 <oog@novicell.dk>
2025-05-14 18:49:29 +0200

    removed some uncommet stuff we need, and also made it so you cant use the "Opret konto" btn without follow our rule for your password

--- src/components/partial/CreateForm.vue
+++ src/components/partial/CreateForm.vue
@@ -26,16 +26,17 @@ const hasUppercase = computed(() => /[A-Z]/.test(password.value));
 const hasNumberOrSymbol = computed(() => /[\d\W]/.test(password.value));
 
 async function handleSubmit() {
-  // !confirmPassword.value
+  !confirmPassword.value
+
   if (!name.value || !email.value || !userName.value || !password.value ) {
     alert("Udfyld venligst alle felter.");
     return;
   }
 
-  // if (password.value !== confirmPassword.value) {
-  //   alert("Adgangskoderne matcher ikke.");
-  //   return;
-  // }
+  if (password.value !== confirmPassword.value) {
+    alert("Adgangskoderne matcher ikke.");
+    return;
+  }
 
   modal.setForm("loading");
 
@@ -48,7 +49,7 @@ async function handleSubmit() {
     }, 3000);
   } else {
     alert(result.error || "Noget gik galt. Prøv igen.");
-    modal.setForm("create"); // Stay on create form if error
+    modal.setForm("create"); 
   }
 }
 </script>
@@ -180,11 +181,14 @@ async function handleSubmit() {
       </div>
 
       <button
+        v-if="hasLowercase && hasUppercase && hasNumberOrSymbol && isLengthValid"
         type="submit"
         class="block mx-auto min-w-32 bg-[var(--color-secondary)] text-black font-semibold py-2 rounded hover:opacity-90 transition"
       >
         Opret konto
       </button>
+
+  
     </form>
     </div>
   </div>
