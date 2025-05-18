9f200e06e785fc5dabe7e9dfc419503249121364
oog1 <oog@novicell.dk>
2025-05-18 16:14:14 +0200

    change fulde navn til --> navn

--- src/components/partial/CreateForm.vue
+++ src/components/partial/CreateForm.vue
@@ -75,13 +75,13 @@ async function handleSubmit() {
     <form @submit.prevent="handleSubmit" class="space-y-4">
 
       <div>
-  <label for="name" class="block text-sm font-medium mb-1">Fulde Navn</label>
+  <label for="name" class="block text-sm font-medium mb-1">Navn</label>
   <input
     id="name"
     v-model="name"
     type="text"
     class="w-full px-4 py-2 rounded border border-gray-300 bg-transparent text-white placeholder-gray-400"
-    placeholder="Indtast dit fulde navn"
+    placeholder="Indtast dit navn"
   />
 </div>
 
