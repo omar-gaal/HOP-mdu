be70cfcd5ae88d9632af75df5b8cc652c6231732
oog1 <oog@novicell.dk>
2025-05-19 16:37:37 +0200

    change interface User deleted name cause it did not match the swagger docs, so name was never and option to store the setUSer

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -5,7 +5,6 @@ import { defineStore } from "pinia";
 interface User {
   key: string;
   email: string;
-  name: string;
   userName: string;
   
 }
@@ -38,11 +37,10 @@ export const useAuthStore = defineStore('auth', {
   }),
   
   actions: {
-    init(): Promise<void> {
+    async  init() {
       return new Promise((resolve) => {
         const authUser = useCookie('authUser');
         const authToken = useCookie('auth');
-    
         try {
           if (authUser.value) {
             this.user = authUser.value as unknown as User;
@@ -56,9 +54,7 @@ export const useAuthStore = defineStore('auth', {
           console.error('Invalid cookie format', error);
           this.clearAuth();
         }
-    
         this.loading = false;
-        resolve();
       });
     },
 
@@ -106,7 +102,6 @@ export const useAuthStore = defineStore('auth', {
           this.setUser({
             key: response.member.key,
             email: response.member.email,
-            name: response.member.name,
             userName: response.member.userName,
           });
 
