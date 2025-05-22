2c96d5ad988a5e1e947ee3ff7d8b8ccda5dfb505
oog1 <oog@novicell.dk>
2025-05-22 22:16:56 +0200

    added modal to global layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -4,9 +4,10 @@ import MobileNav from "~/Navigation/MobileNav.vue";
 import TheFooter from "~/components/global/TheFooter.vue";
 import { useWindowSize } from "#imports";
 import { useAuthStore } from '@/stores/auth';
-
+import { useModal } from "#imports";
 const { width } = useWindowSize();
 const isMobile = computed(() => width.value < 1080);
+const modal = useModal()
 </script>
 
 <template>
@@ -19,11 +20,20 @@ const isMobile = computed(() => width.value < 1080);
       <MobileNav v-else />
     </div>
 
+    
+
    
     <main class="flex-grow pb-64 sm:pb-20">
       <slot />
     </main>
 
+    <BaseModal>
+      <PartialLoginForm v-if="modal.activeForm === 'login'" />
+      <PartialCreateForm v-else-if="modal.activeForm === 'create'" />
+      <LoadingMessage v-else-if="modal.activeForm === 'loading'" />
+      <SuccessMessage v-else-if="modal.activeForm === 'success'" />
+      <ForgotPasswordForm v-else-if="modal.activeForm === 'forgot'" />
+    </BaseModal>
     
     <TheFooter />
   </div>
