695738847a26537148ce5df42bec07e67e1dd194
oog1 <oog@novicell.dk>
2025-05-22 22:16:36 +0200

    Move Modal to be used Global

--- src/components/views/FrontPage.vue
+++ src/components/views/FrontPage.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
 import BaseHero from "~/components/base/BaseHero.vue";
 import ProductList from "~/components/partial/ProductList.vue";
-
-import PartialLoginForm from "@/components/partial/LoginForm.vue";
-import PartialCreateForm from "@/components/partial/CreateForm.vue";
-import LoadingMessage from "@/components/partial/LoadingMessage.vue";
-import ForgotPasswordForm from "@/components/partial/ForgotPasswordForm.vue";
-import SuccessMessage from "@/components/partial/successMessage.vue";
-import { useModal } from "@/stores/modal";
 import { useFavoritesStore } from "#imports";
 import { productData } from "~/data/productData";
 
-const modal = useModal();
+
 const favorites = useFavoritesStore();
 </script>
 
@@ -24,13 +17,6 @@ const favorites = useFavoritesStore();
   
     </BaseContainer>
 
-    <BaseModal>
-      <PartialLoginForm v-if="modal.activeForm === 'login'" />
-      <PartialCreateForm v-else-if="modal.activeForm === 'create'" />
-      <LoadingMessage v-else-if="modal.activeForm === 'loading'" />
-      <SuccessMessage v-else-if="modal.activeForm === 'success'" />
-      <ForgotPasswordForm v-else-if="modal.activeForm === 'forgot'" />
-    </BaseModal>
 
     <BaseContainer :isFrontpage="true">
       <section class="bg-white w-full py-6">
